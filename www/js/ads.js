4d98b074b72546c5d5c27ff9feafc2e197fd70dd
Ahmed Mansour <atphalix@inbox.com>
2012-12-28 14:00:11 +0000

    First pages commit

---
+++ www/js/ads.js
@@ -0,0 +1,25 @@
+/*
+ * JavaScript file
+ */
+
+var innerActive_aid = 'eduapps_PatisserieMarocaine_Android';
+var innerActive_test = 'true';
+innerActive_ad_params.age = '25';
+innerActive_ad_params.gender = 'f';
+
+function init()
+{	
+	getInnerActiveAd("banner1","");
+	getInnerActiveAd("banner2","showAdText");
+	createInnerActiveSplashAd();	
+
+	var adClick = document.getElementById("toggle");
+	adClick.onclick = function(){
+		showSplash();
+	}
+
+	var adClick1 = document.getElementById("toggle1");
+	adClick1.onclick = function(){
+		showSplash(10);
+	}
+}
